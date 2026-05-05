adc436fb279da0b58e0501bfb1c78dcfde413528
anoca1011 <anoca1011@gmail.com>
2026-05-05 08:41:42 -0300

    Conexão com o banco via PDO

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+function db(): PDO
+{
+    $host = '127.0.0.1';
+    $dbname = 'crud_php';
+    $user = 'root';
+    $pass = '';
+
+    $dsn = "mysql:host=$host;dbname;charset=utf8mb4";
+
+    $pdo = new PDO ($dsn, $user, $pass,[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE =>PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+
+    return $pdo;
+}
