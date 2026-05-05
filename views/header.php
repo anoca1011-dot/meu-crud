b7cdd4fd0f02d0f6236b7351490756444b8d668b
anoca1011 <anoca1011@gmail.com>
2026-05-05 11:33:24 -0300

    inconpleto 6.1

---
+++ views/header.php
@@ -0,0 +1,15 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD tarefas</title>
+    <style>
+        body {font-family:Arial, sens-serif; margin: 24px; max-width: 900px; }
+        table{border-collapse:}
+    </style>
+</head>
+<body>
+</body>
+</html>
